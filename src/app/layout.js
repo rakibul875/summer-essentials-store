1fbf44135a110ef3920c46501887eae6faea5a88
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-29 20:26:54 +0600

    Added NavLink component for navigation links with active state styling.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/shared/NavBar";
+import Footer from "@/components/shared/Footer";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,9 +23,16 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme='light'
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <NavBar />
+        <main>
+          {children}
+        </main>
+        <Footer/>
+      </body>
     </html>
   );
 }
---
+++ src/components/shared/NavBar.jsx
@@ -0,0 +1,73 @@
+import Link from "next/link";
+import React from "react";
+import NavLink from "./NavLink";
+
+const NavBar = () => {
+  const links = (
+    <>
+      <li className="font-semibold text-lg">
+        <NavLink href={"/"}>Home</NavLink>
+      </li>
+      <li className="font-semibold text-lg">
+        <NavLink href={"/product"}>Product</NavLink>
+      </li>
+      <li className="font-semibold text-lg">
+        <NavLink href={"/my-profile"}>My Profile</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="bg-base-100 shadow-sm">
+      <div className="navbar container mx-auto">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
+          </div>
+          <Link
+            href={"/"}
+            className="btn btn-ghost text-orange-500 font-bold text-2xl"
+          >
+            SOLSTICE
+          </Link>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
+        </div>
+        <div className="navbar-end gap-2">
+          <Link href={''}>
+            <button className="btn border border-orange-500 text-orange-500 rounded-sm">Login</button>
+          </Link>
+          <Link href={''}>
+            <button className="btn bg-orange-500 text-white rounded-sm">Register</button>
+          </Link>
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
