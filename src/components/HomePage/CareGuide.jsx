3bfd24801ff90703ccca415a39cdf6b330b4f3c4
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-30 12:01:46 +0600

    Refactor code structure for improved readability and maintainability

--- src/components/HomePage/CareGuide.jsx
+++ src/components/HomePage/CareGuide.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import girlImage from "@/assets/image/woman.jpg";
 import Image from "next/image";
+import AloVra from "@/assets/image/aloVra.jpg";
+import { MdOpacity, MdOutlineWbSunny } from "react-icons/md";
+import { FaEye } from "react-icons/fa6";
+import { TbTemperatureSun } from "react-icons/tb";
 
 const CareGuide = () => {
   return (
@@ -17,6 +21,7 @@ const CareGuide = () => {
         <div className="flex gap-5 mt-5 flex-col lg:flex-row">
           <div className="card bg-base-100 card-xl shadow-sm">
             <div className="card-body">
+              <h1 className="bg-blue-100 w-10 flex justify-center text-blue-400 rounded-full px-1 py-2"><MdOpacity size={'1rem'}/></h1>
               <h2 className="card-title">Deep Hydration</h2>
               <p>
                 Hydrate from within. Aim for 3 liters of <br /> mineral-infused
@@ -24,16 +29,16 @@ const CareGuide = () => {
                 levels.
               </p>
               <div className="justify-start card-actions">
-                <button className="btn hover:bg-orange-500 hover:text-white btn-outline rounded-xl border-orange-500 text-orange-500">Read Guide</button>
+                <button className="btn hover:bg-orange-500 hover:text-white btn-outline rounded-xl border-orange-500 text-orange-500">
+                  Read Guide
+                </button>
               </div>
             </div>
           </div>
           <div className="">
-            <div className="card flex-col lg:flex-row card-side bg-base-100 shadow-sm h-full">
-              <figure>
-                <Image src={girlImage} alt="Movie" width={360} height={360} />
-              </figure>
+            <div className="card flex-col-reverse lg:flex-row card-side bg-base-100 shadow-sm h-full">
               <div className="card-body">
+                 <h1 className="bg-orange-200 text-orange-500 rounded-full flex justify-center w-10 px-1 py-2"><MdOutlineWbSunny size={'1rem'}/></h1>
                 <h2 className="card-title">SPF is Your Best Friend</h2>
                 <p className="text-xl">
                   Broad-spectrum protection is non- <br />
@@ -43,6 +48,45 @@ const CareGuide = () => {
                 <div className="card-actions justify-start">
                   <button className="btn bg-slate-800 text-white hover:bg-amber-500 rounded-md">
                     Explore SPF Collection
+                  </button>
+                </div>
+              </div>
+              <figure>
+                <Image src={girlImage} alt="Movie" width={360} height={360} />
+              </figure>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-5 mt-10">
+          <div className="">
+            <div className="card flex-col-reverse lg:flex-row card-side bg-base-100 shadow-sm h-full">
+              <div className="card-body">
+               <h1 className="bg-blue-100 w-10 flex justify-center text-blue-400 rounded-full px-1 py-2"><TbTemperatureSun size={'1rem'} /></h1>
+                <h2 className="card-title">After-Sun Recovery</h2>
+                <p className="text-xl">
+                  Cool down with aloe-infused mists <br /> and lightweight
+                  serums to soothe skin after a long day in the heat.
+                </p>
+              </div>
+              <figure>
+                <Image src={AloVra} alt="Movie" width={360} height={360} />
+              </figure>
+            </div>
+          </div>
+          <div className="">
+            <div className="card bg-base-100 card-xl shadow-sm">
+              <div className="card-body">
+                <h1 className="bg-orange-200 text-orange-500 rounded-full flex justify-center w-10 px-1 py-2">
+                  <FaEye size={'1rem'}/>
+                </h1>
+                <h2 className="card-title">Eye Health</h2>
+                <p>
+                  Protect your vision from intense UV rays with polarized lenses
+                  and UV-coated optics.
+                </p>
+                <div className="justify-start card-actions">
+                  <button className="btn hover:bg-orange-500 hover:text-white btn-outline rounded-xl border-orange-500 text-orange-500">
+                    Shop Eyewear
                   </button>
                 </div>
               </div>
