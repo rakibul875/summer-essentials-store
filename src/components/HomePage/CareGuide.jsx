c834f226756c82b7d9b73d82d4a7428eac89740a
rakibul875 <rakibulislamashik78@gmail.com>
2026-04-30 10:03:12 +0600

    Update CareGuide component with new content and styling adjustments

--- src/components/HomePage/CareGuide.jsx
+++ src/components/HomePage/CareGuide.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import girlImage from '@/assets/image/woman.jpg'
+import girlImage from "@/assets/image/woman.jpg";
 import Image from "next/image";
 
 const CareGuide = () => {
   return (
-    <div className="bg-blue-200 p-10 mt-10">
+    <div className="bg-blue-200 p-4 lg:p-10 mt-10">
       <div className="text-center">
         <h1 className="text-2xl">Summer Care Guide</h1>
         <p className="text-xl">
@@ -15,33 +15,35 @@ const CareGuide = () => {
 
       <div className="lg:mx-10">
         <div className="flex gap-5 mt-5 flex-col lg:flex-row">
-            <div className="card bg-base-100 card-xl shadow-sm">
-              <div className="card-body">
-                <h2 className="card-title">Xlarge Card</h2>
-                <p>
-                  A card component has a figure, a body part, and inside body
-                  there are title and actions parts
-                </p>
-                <div className="justify-end card-actions">
-                  <button className="btn btn-primary">Buy Now</button>
-                </div>
+          <div className="card bg-base-100 card-xl shadow-sm">
+            <div className="card-body">
+              <h2 className="card-title">Deep Hydration</h2>
+              <p>
+                Hydrate from within. Aim for 3 liters of <br /> mineral-infused
+                water daily to maintain skin <br /> elasticity and energy
+                levels.
+              </p>
+              <div className="justify-start card-actions">
+                <button className="btn hover:bg-orange-500 hover:text-white btn-outline rounded-xl border-orange-500 text-orange-500">Read Guide</button>
               </div>
             </div>
+          </div>
           <div className="">
-            <div className="card flex-col lg:flex-row card-side bg-base-100 shadow-sm">
+            <div className="card flex-col lg:flex-row card-side bg-base-100 shadow-sm h-full">
               <figure>
-                <Image
-                  src={girlImage}
-                  alt="Movie"
-                  width={360}
-                  height={360}
-                />
+                <Image src={girlImage} alt="Movie" width={360} height={360} />
               </figure>
               <div className="card-body">
-                <h2 className="card-title">New movie is released!</h2>
-                <p>Click the button to watch on Jetflix app.</p>
-                <div className="card-actions justify-end">
-                  <button className="btn btn-primary">Watch</button>
+                <h2 className="card-title">SPF is Your Best Friend</h2>
+                <p className="text-xl">
+                  Broad-spectrum protection is non- <br />
+                  negotiable. Reapply every two <br /> hours, especially after a
+                  swim or sunbathing session.
+                </p>
+                <div className="card-actions justify-start">
+                  <button className="btn bg-slate-800 text-white hover:bg-amber-500 rounded-md">
+                    Explore SPF Collection
+                  </button>
                 </div>
               </div>
             </div>
