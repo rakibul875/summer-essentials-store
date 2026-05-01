f8a705a358a5a2db7aac265f5bea46fa6264c403
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-01 10:07:52 +0600

    Mongodb uri setup

--- src/app/product/[productId]/loading.jsx
+++ src/app/product/[productId]/loading.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const loading = () => {
     return (
         <div className='container mx-auto flex justify-center items-center m-auto min-h-screen'>
-            <span className="loading loading-spinner text-orange-600 loading-xl"></span>
+            <span className="loading loading-spinner text-orange-500 loading-xl"></span>
         </div>
     );
 };
