0158cc6d59c72515c9c510551fd18e27c6087a8d
rakibul875 <rakibulislamashik78@gmail.com>
2026-05-02 09:49:21 +0600

    Install hero Ui

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { Button } from "@heroui/react";
+import React from "react";
 
 const page = () => {
-    return (
-        <div>
-            profile
-        </div>
-    );
+  return (
+    <div>
+      <Button>My Button</Button>
+      <button className="btn btn-xl btn-primary">Hello</button>
+    </div>
+  );
 };
 
 export default page;
